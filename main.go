eadd5e4b80d373f618bd95d0251e98c8dc86f939
Mark <meichangliang@outlook.com>
2020-04-14 19:35:21 +0800

    ~

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func main() {
-	output()
+type DebugInfo struct {
+	url string
 }
 
-func output() {
-	fmt.Println("hell world")
+func main() {
+	data := httpGet("https://cn.bing.com/HPImageArchive.aspx?format=js&n=10")
+	toJson(data)
+}
 
+func (dbgInfo DebugInfo) String() string {
+	return fmt.Sprintf("{url: %s,}", dbgInfo.url)
+}
+
+func toJson(dataStr string) {
+	data := dataStr
+
+	var dbgInfos []DebugInfo
+	json.Unmarshal([]byte(data), &dbgInfos)
+
+	fmt.Println(dbgInfos)
+}
+
+func httpGet(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	return string(body)
 }
