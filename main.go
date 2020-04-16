f38447fce916776d16328755d2a1c2f0bfa63d62
Mark <meichangliang@outlook.com>
2020-04-16 13:09:33 +0800

    update ~

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	bingApi "bingImg.com/bingApi"
+	service "bingImg.com/service"
 )
 
 func main() {
 	bingApi.GetData()
 	// var data = bingApi.JsonArr
+
+	service.Start()
 }
