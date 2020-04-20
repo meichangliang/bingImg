694273aa326d3b4bd753fc742dd5503addcba0c0
Mark <meichangliang@outlook.com>
2020-04-20 21:48:19 +0800

    update~

---
+++ downImg/index.go
@@ -0,0 +1,8 @@
+package getImgData
+
+import "fmt"
+
+//获取 bingImg
+func GetImg(url string) {
+	fmt.Println("asd", url)
+}
---
+++ downImg/index_test.go
@@ -0,0 +1,10 @@
+package getImgData
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	GetImg("https://cn.bing.com/th?id=OHR.WatChaloem_ZH-CN8722271527_1920x1080.jpg")
+}
