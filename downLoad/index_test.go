0cfcf380ccf1c70417e2430cf02cdb51ff20a138
Mark <meichangliang@outlook.com>
2020-04-27 13:31:59 +0800

    add

--- downLoad/index_test.go
+++ downLoad/index_test.go
@@ -6,5 +6,5 @@ import (
 
 func TestSum(t *testing.T) {
 
-	GetImg("https://cn.bing.com/th?id=OHR.WatChaloem_ZH-CN8722271527_1920x1080.jpg")
+	GetImg("https://cn.bing.com/th?id=OHR.WatChaloem_ZH-CN8722271527_1920x1080.jpg", "./images")
 }
